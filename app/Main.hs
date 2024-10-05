9789f7a95b624b3c22e294b560a46858f3f58d8a
Mason Hall <hallmason17@gmail.com>
2024-10-04 22:52:12 -0400

    add git branch if in a repo

--- app/Main.hs
+++ app/Main.hs
@@ -1,17 +1,30 @@
 module Main where
 
-import Control.Exception (IOException, handle)
+import Control.Exception (IOException, handle, try)
 import Control.Monad (forever)
-import Data.Text
+import Data.List.Split (splitOn)
+import Data.Text (Text, append, pack, unpack)
 import qualified Data.Text.IO as TIO
 import System.Environment (getEnv)
-import System.IO (hFlush, stdout)
+import System.IO
+import System.IO.Error (isDoesNotExistError)
 import System.Process (callProcess)
 
 data Action = Command Text [Text]
 
 handler :: IOException -> IO ()
 handler _ = putStrLn "Invalid command, try again."
+
+readHandler :: IOError -> IO String
+readHandler e
+  | isDoesNotExistError e = do putStrLn "No git repository found in this directory."; return ""
+  | otherwise = do putStrLn "An error occurred."; return ""
+
+getCurrentBranchIfExist :: FilePath -> IO String
+getCurrentBranchIfExist cwd = do
+  let gitDir = cwd ++ "/.git/HEAD"
+  contents <- handle readHandler $ readFile gitDir
+  return $ Prelude.head $ splitOn "\n" $ Prelude.last $ splitOn "/" contents
 
 logCommandToFile :: Text -> IO ()
 logCommandToFile cmd = do
@@ -31,7 +44,10 @@ maybeRunCommand cmd args = do
 
 prompt :: IO Text
 prompt = do
-  putStr "\ESC[93m~~> \ESC[0m"
+  maybeBranch <- try (getCurrentBranchIfExist ".") :: IO (Either IOException String)
+  case maybeBranch of
+    Left _ -> putStr "\ESC[93m~~> \ESC[0m"
+    Right branch -> putStr ("\ESC[93m~~> \ESC[30m" ++ "(" ++ branch ++ ")" ++ "\ESC[0m")
   hFlush stdout
   TIO.getLine
 
